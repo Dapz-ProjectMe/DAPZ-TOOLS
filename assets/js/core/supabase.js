8cda7ce15dc049650e9a4c316e12a72b1130e5a3
Dapz-ProjectMe <budi060722@gmail.com>
2026-09-12 12:19:16 +0700

    Update supabase.js

--- assets/js/core/supabase.js
+++ assets/js/core/supabase.js
@@ -3,8 +3,8 @@
 // NEVER place the service_role key in frontend code.
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-export const SUPABASE_URL = 'PASTE_SUPABASE_URL_HERE';
-export const SUPABASE_PUBLISHABLE_KEY = 'PASTE_SUPABASE_PUBLISHABLE_KEY_HERE';
+export const SUPABASE_URL = 'https://xlxufliwijblnauzhkko.supabase.co';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_aiOD7NYtz3ORz9qdaVsFXA_lATIGZjr';
 
 export const supabase = createClient(
   SUPABASE_URL,
